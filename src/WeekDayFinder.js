fdc64ea3aae9447c0d996356e59c196f28e88d7e
JeffDinsmore <hello@jeffdinsmore.com>
2022-02-02 20:04:50 -0800

    upadted error catching for months and days

--- src/WeekDayFinder.js
+++ src/WeekDayFinder.js
@@ -2,12 +2,35 @@ import React, { useState } from 'react';
 import WeekdayChecker from './WeekdayChecker';
 
 function WeekDayFinder() {
+  const thirtyDays = {'2': true, '4': true, '7': true, '9': true};
+  const monthArray = ['March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February'];
+  //console.log(thirtyDays['4'])
   const [properties, setProperties] = useState({});
   function handleFormSubmit(e) {
     e.preventDefault();
-    let years = e.target.yearInput.value;
-    if(years.length === 1) {
-      years = "000" + years;
+    console.log(thirtyDays[e.target.monthInput.value], "joe", parseInt(e.target.dayInput.value))
+    if(e.target.monthInput.value === '12' && parseInt(e.target.dayInput.value) > 28) {
+      alert("There are only 28 days in February");
+    } else if(thirtyDays[e.target.monthInput.value] === true && parseInt(e.target.dayInput.value) > 30) {
+      alert("There are only 30 days in " + monthArray[parseInt(e.target.monthInput.value)-1]);
+    } 
+    else {
+      let years = e.target.yearInput.value;
+      if(years.length === 1) {
+        let years = e.target.yearInput.value;
+      if(years.length === 1) {
+        years = "000" + years;
+      } else if(years.length === 2) {
+        years = "00" + years;
+      } else if(years.length === 3) {
+        years = "0" + years;
+      }
+    const properties = {
+      month: e.target.monthInput.value,
+      day: e.target.dayInput.value,
+      year: years,
+    }
+    setProperties(properties);    years = "000" + years;
     } else if(years.length === 2) {
       years = "00" + years;
     } else if(years.length === 3) {
@@ -19,6 +42,8 @@ function WeekDayFinder() {
       year: years,
     }
     setProperties(properties);
+    }
+    
   }
   return (
     <React.Fragment>
