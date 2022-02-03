aaaeebb6a29434a6830f06255961964e92813cc6
JeffDinsmore <hello@jeffdinsmore.com>
2022-02-02 23:49:02 -0800

    updated error handling to work properly

--- src/WeekDayFinder.js
+++ src/WeekDayFinder.js
@@ -2,37 +2,29 @@ import React, { useState } from 'react';
 import WeekdayChecker from './WeekdayChecker';
 
 function WeekDayFinder() {
-  const thirtyDays = {'2': true, '4': true, '7': true, '9': true};
-  const monthArray = ['March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February'];
-  //console.log(thirtyDays['4'])
+
   const [properties, setProperties] = useState({});
   function handleFormSubmit(e) {
     e.preventDefault();
-    console.log(thirtyDays[e.target.monthInput.value], "joe", parseInt(e.target.dayInput.value))
-    if(e.target.monthInput.value === '12' && parseInt(e.target.dayInput.value) > 28) {
-      alert("There are only 28 days in February");
-    } else if(thirtyDays[e.target.monthInput.value] === true && parseInt(e.target.dayInput.value) > 30) {
-      alert("There are only 30 days in " + monthArray[parseInt(e.target.monthInput.value)-1]);
-    } else {
-        let years = e.target.yearInput.value;
-        if(years.length === 1) {
-          let years = e.target.yearInput.value;
-        if(years.length === 1) {
-          years = "000" + years;
-        } else if(years.length === 2) {
-          years = "00" + years;
-        } else if(years.length === 3) {
-          years = "0" + years;
-        }
-        const properties = {
-          month: e.target.monthInput.value,
-          day: e.target.dayInput.value,
-          year: years,
+    let years = e.target.yearInput.value;
+    if (years.length === 1) {
+      let years = e.target.yearInput.value;
+      if (years.length === 1) {
+        years = "000" + years;
+      } else if (years.length === 2) {
+        years = "00" + years;
+      } else if (years.length === 3) {
+        years = "0" + years;
       }
-    setProperties(properties);    years = "000" + years;
-    } else if(years.length === 2) {
+      const properties = {
+        month: e.target.monthInput.value,
+        day: e.target.dayInput.value,
+        year: years,
+      }
+      setProperties(properties); years = "000" + years;
+    } else if (years.length === 2) {
       years = "00" + years;
-    } else if(years.length === 3) {
+    } else if (years.length === 3) {
       years = "0" + years;
     }
     const properties = {
@@ -41,9 +33,8 @@ function WeekDayFinder() {
       year: years,
     }
     setProperties(properties);
-    }
-    
   }
+  
   return (
     <React.Fragment>
       <div className="container">
@@ -72,7 +63,7 @@ function WeekDayFinder() {
           <input
             type="number"
             name='dayInput'
-            id="dayInput" 
+            id="dayInput"
             min="1"
             max="31"
             required={true}
@@ -89,7 +80,7 @@ function WeekDayFinder() {
           <br></br><br />
           <button type="submit" className="btn btn-info">What day of the week was it?</button>
         </form>
-        <WeekdayChecker month={properties.month} day={properties.day} year={properties.year}/>
+        <WeekdayChecker month={properties.month} day={properties.day} year={properties.year} />
       </div>
     </React.Fragment>
   );
--- src/WeekdayChecker.js
+++ src/WeekdayChecker.js
@@ -5,7 +5,16 @@ function WeekdayChecker(props) {
   const { day, month, year } = props;
   const weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const monthArray = ['March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February'];
-  
+  const thirtyDays = { '2': true, '4': true, '7': true, '9': true };
+  let errorMessage = '';
+  if (month === '12' && parseInt(day) > 28 && !checkLeapYear()) {
+    errorMessage = "There are only 28 days in February in a non-leap-year. Please pick again.";
+  } else if (thirtyDays[month] === true && parseInt(day) > 30) {
+    errorMessage = "There are only 30 days in " + monthArray[parseInt(month) - 1] + ". Please pick again.";
+  } else if (month === '12' && parseInt(day) > 29 && checkLeapYear()) {
+    errorMessage = "There are only 29 days in February in a leap year. Please pick again.";
+  }
+
   function zellersEquation() {
     if (day && day > 0 && month && month > 0 && year && year >= 0) {
       //let years = 
@@ -42,10 +51,13 @@ function WeekdayChecker(props) {
   return (
     <React.Fragment>
       <br />
-      <h1 style={{ display: day && year && month ? 'block' : 'none' }}><strong>{weekdayArray[zellersEquation()]}</strong></h1>
+      <div style={{ display: errorMessage !== "" ? 'block' : 'none', backgroundColor: '#FFCCCC', border: '2px solid #FF8888', padding: '10px', borderRadius: '5px' }}>
+        <h3 style={{ display: errorMessage !== "" ? 'block' : 'none' }}>{errorMessage}</h3>
+      </div>
+      <h1 style={{ display: day && year && month && errorMessage === '' ? 'block' : 'none' }}><strong>{weekdayArray[zellersEquation()]}</strong></h1>
       <br />
-      <p style={{ display: day && year && month ? 'block' : 'none' }}>The day of the week that {monthArray[parseInt(month) - 1]} {day}, {parseInt(year).toString()} falls on is <strong>{weekdayArray[zellersEquation()]}</strong>.</p>
-      <p style={{ display: checkLeapYear() ? 'block' : 'none' }}>This year falls on a <strong>Leap Year</strong>!</p>
+      <p style={{ display: day && year && month && errorMessage === '' ? 'block' : 'none' }}>The day of the week that {monthArray[parseInt(month) - 1]} {day}, {parseInt(year).toString()} falls on is <strong>{weekdayArray[zellersEquation()]}</strong>.</p>
+      <p style={{ display: checkLeapYear() && errorMessage === '' ? 'block' : 'none' }}>This year falls on a <strong>Leap Year</strong>!</p>
     </React.Fragment>
   );
 }
